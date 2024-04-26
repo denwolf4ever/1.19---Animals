0e35fcc80a3ce4bf3f725b0e7b49276447a8fb03
denwolf4ever <denwolf4ever@gmail.com>
2024-04-26 12:52:01 +0300

    first commit

---
+++ HW_19.cpp
@@ -0,0 +1,64 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class Animal 
+{
+public:
+	virtual void Voice()
+	{
+		cout << "voice" << endl;
+	}
+	
+
+private:
+
+};
+class Cat : public Animal
+{
+public:
+	void Voice() override
+	{
+		cout << "Meow" << endl;
+	}
+private:
+	
+};
+class Dog : public Animal
+{
+public:
+	void Voice() override
+	{
+		cout << "Woof" << endl;
+	}
+private:
+
+};
+class Fox : public Animal
+{
+public:
+	void Voice() override
+	{
+		cout << "Ring-ding-ding-ding-dingeringeding! Gering - ding - ding - ding - dingeringeding! Gering - ding - ding - ding - dingeringeding" << endl;
+	}
+private:
+
+};
+
+
+
+int main()
+{
+	Animal* arr = new Animal[8]
+	{
+		Cat(), Dog(), Fox(), Dog(), Fox(), Dog(), Cat(), Cat()
+	};
+	for (int i = 0; i < 8; i++)
+	{
+		arr[i].Voice();
+	}
+	Cat neko;
+	neko.Voice();
+	delete[] arr;
+}
+
